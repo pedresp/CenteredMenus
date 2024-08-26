42888f3853b207a7ffa5b78a6264556bb6088329
pedresp <pedro.espinosa@alumnos.upm.es>
2024-08-26 09:53:42 +0200

    increased number of options in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@ int main()
     initscr();
     start_color();
 
-    std::vector<std::string> options = {"option A","option B","option C","option D","option E","option F"};
-    cms::centered_menu("CENTERED MENU", options);
+    std::vector<std::string> options = {"option A","option B","option C","option D","option E","option F","option G","option H","option I","option J","option K","option L"};
+    int option_selected = cms::centered_menu("CENTERED MENU", options);
+
+    mvprintw(LINES/2, COLS/2, "%d", option_selected);
+    getch();
 
     endwin();
     return 0;
